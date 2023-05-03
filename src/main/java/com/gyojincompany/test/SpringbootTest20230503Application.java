c645625b80fdb47b7f2830f3a0de4c98f34cf27c
ICI <ICI@DESKTOP-EA5M9P6>
2023-05-03 15:56:35 +0900

    springboot test

---
+++ src/main/java/com/gyojincompany/test/SpringbootTest20230503Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTest20230503Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTest20230503Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/test/SpringbootTest20230503ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTest20230503ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
